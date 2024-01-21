f6eb89c1076a3d2913379fbc82199eac7186fc61
FlynntKnapp <bruce.stull@gmail.com>
2024-01-21 13:57:19 -0800

    Updated return strings

--- between_ten_and_twenty.py
+++ between_ten_and_twenty.py
@@ -1,37 +1,47 @@
-class GreaterThanTwentyError(Exception):
+class GreaterThanTwentyException(Exception):
     """Exception raised for errors in the input value being greater than 20."""
+
     def __init__(self, value):
         self.value = value
-        self.message = f"The value {value} is greater than 20."
+        self.message = (
+            f"{self.__class__.__name__}: The input value {value} is greater than 20."
+        )
         super().__init__(self.message)
 
-class LessThanTenError(Exception):
+
+class LessThanTenException(Exception):
     """Exception raised for errors in the input value being less than 10."""
+
     def __init__(self, value):
         self.value = value
-        self.message = f"The value {value} is less than 10."
+        self.message = (
+            f"{self.__class__.__name__}: The input value {value} is less than 10."
+        )
         super().__init__(self.message)
 
-def check_value(value):
+
+def check_value_between_10_20(value):
     if value > 20:
-        raise GreaterThanTwentyError(value)
+        raise GreaterThanTwentyException(value)
     elif value < 10:
-        raise LessThanTenError(value)
+        raise LessThanTenException(value)
     else:
-        return "Value is between 10 and 20."
+        return f"The input value {value} is between 10 and 20."
+
 
 # Example usage
 try:
-    print(check_value(25))
+    print("check_value_between_10_20(25): ", check_value_between_10_20(25))
+except Exception as e:
+    print(e)
+
+
+try:
+    print("check_value_between_10_20(5): ", check_value_between_10_20(5))
 except Exception as e:
     print(e)
 
 try:
-    print(check_value(5))
-except Exception as e:
-    print(e)
-
-try:
-    print(check_value(15))
+    print("check_value_between_10_20(15): ", check_value_between_10_20(15))
 except Exception as e:
     print(e)
